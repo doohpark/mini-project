ac132cc6fad580a60237e7c10155beb849db19ea
doohpark <doohyun2120@gmail.com>
2021-04-18 03:43:06 +0900

    test9

--- product.h
+++ product.h
@@ -10,7 +10,7 @@ typedef struct{
 
 int addProduct(Product *p); //제품을 추가하는 함수
 void readProduct(Product p); //제품을 출력하는 함수
-int updateProduct(Product *p); // 특정 제품을 수정하는 함수
-int deleteProduct(Product *p); // 특정 제품을 삭제하는 함수
+int updateProduct(Product *p); //  제품을 수정하는 함수
+int deleteProduct(Product *p); //  제품을 삭제하는 함수
 
 #endif
